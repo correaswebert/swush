44e1337ddd8d34c5fd168c64e31d6c8f9c516064
Aayush <aayushmum@gmail.com>
2021-04-10 23:02:16 +0530

    fixed view teams api

--- client/pages/api/team/view.js
+++ client/pages/api/team/view.js
@@ -12,7 +12,7 @@ export default async (req, res) => {
 
     /* get the teams of the user */
     const teams = await user.populate('teams._id').execPopulate();
-    
+  
     res.status(200).json(teams.teams);
   } catch (error) {
     res.status(500).json({ Error: 'Internal server error!' });
