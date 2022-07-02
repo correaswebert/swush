ec10372d1adc723cdff4c17b3eae4aa2a1236357
Aayush <aayushmum@gmail.com>
2022-07-02 17:07:13 +0530

    deleted unnecessary folder

--- server/app/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const router = Router();
-
-router.get("/", (_req, res) => {
-  res.send("Swush | A secure vault for teams.");
-});
-
-export { router as indexRouter };
